0c645b0f56f6ce186f9cd927fa2d516edcb75c9d
Tony Spiro <tspiro@tonyspiro.com>
2024-12-08 15:29:56 -0800

    edit: zoom og image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,10 +3,11 @@ import { ImageResponse } from "next/og";
 
 export default async function Image() {
   const homePage = await getHomePage();
+  console.log(homePage.metadata.main_image.imgix_url);
   return new ImageResponse(
     (
       <img
-        src={`${homePage.metadata.main_image.imgix_url}?w=1200&auto=compress`}
+        src={`${homePage.metadata.main_image.imgix_url}?w=1000&h=600&fit=crop`}
       />
     )
   );
